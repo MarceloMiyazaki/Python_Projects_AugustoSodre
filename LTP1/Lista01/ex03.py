fb1b6829eb56d4a128d20a339ab1c3bcdd417438
Augusto.Sodre <augustolima73@yahoo.com>
2024-08-30 20:55:13 -0300

    Fibonacci Final

--- LTP1/Lista01/ex03.py
+++ LTP1/Lista01/ex03.py
@@ -2,14 +2,14 @@
 Escreva um programa que imprima os primeiros 10 números da sequência de Fibonacci.
 '''
 
-'Os dois primeiros termos da Sequência de Fibonacci, 0 e 1, são fixos'
+#Os dois primeiros termos da Sequência de Fibonacci, 0 e 1, são fixos!'
 a = 0
 b = 1
 
 print(a)
 print(b)
 
-for i in range(10):
+for i in range(8):
     temp = a + b
     print(temp)
     a = b
