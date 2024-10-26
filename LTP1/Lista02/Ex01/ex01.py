2ed8770c8ab03d2382ec577983a371bf2a42ef07
Augusto.Sodre <augustolima73@yahoo.com>
2024-10-26 11:30:52 -0300

    Ex 01 Final

---
+++ LTP1/Lista02/Ex01/ex01.py
@@ -0,0 +1,57 @@
+import turtle as tl
+
+#Classe Base
+class Forma():
+
+    def desenhar(self, forma):
+        #Método abstrato
+        pass
+
+
+#Classe da forma de círculo
+class Circulo(Forma):
+
+    def __init__(self, forma):
+        self.forma = forma
+
+    def desenhar(self):
+        tl.color("Red")
+
+        #Cria um círculo
+        
+        tl.circle(50)
+
+        print("Desenhou um " + self.forma)
+
+
+#Classe da forma de quadrado
+class Quadrado(Forma):
+
+    def __init__(self, forma):
+        self.forma = forma
+
+    def desenhar(self):
+        tl.color("Black")
+
+        #Cria um quadrado
+        for i in range(4):
+            tl.forward(100)
+            tl.right(90)
+        
+        print("Desenhou um " + self.forma)
+
+
+#Criando objetos de acordo com a forma
+forma_1 = Quadrado("Quadrado")
+forma_2 = Circulo("Circulo")
+
+#Configurando a caneta do Turtle
+tl.pensize(3)
+tl.speed(1)
+
+#Invocando métodos
+forma_1.desenhar()
+forma_2.desenhar()
+
+#Finalizando a Turtle
+tl.done()
